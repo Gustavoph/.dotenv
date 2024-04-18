117cccd285a5f4b7cbbf89ce88c522ae93de2393
gustavoph <gusta.coliveira07@gmail.com>
2024-04-18 18:02:46 -0300

    feat: add new nvim configs

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,19 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup({
+      options = {
+        theme = 'onedark'
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {'hostname'}
+      },
+    })
+  end
+}
