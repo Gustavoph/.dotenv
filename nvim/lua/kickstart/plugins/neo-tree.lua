d853e44c092d9c5624058efe720c042730fe5e5e
Gustavo Oliveira <gusta.coliveira07@gmail.com>
2024-06-18 00:20:48 -0300

    Atualizando configurações do nvim e do tmux

---
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,23 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '<C-p>', ':Neotree toggle float<CR>', silent = true, { desc = 'Float File Explorer' } },
+    { '\\', ':Neotree reveal<CR>', silent = true, { desc = 'NeoTree reveal' } },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
